db14210e5931e4e42fb19121c82eb6e9afff0a98
Andrick Siegmund <andrick.siegmund@gmail.com>
2024-01-16 16:20:59 -0700

    fix job sort to show 'rejected', 'declined', or 'withdrew' at the end of the table

--- client/src/utils/sortJobs.js
+++ client/src/utils/sortJobs.js
@@ -4,15 +4,15 @@ import utc from "dayjs/plugin/utc"
 dayjs.extend(utc)
   
   const sortJobs = (a, b) => {
-  const statusOrder = [ 'Rejected', 'Withdrew']
+  const statusOrder = [ 'Rejected', 'Declined', 'Withdrew']
 
   // this checks if either job has a status of rejected or withdrew
   const aStatusIndex = statusOrder.indexOf(a.status)
   const bStatusIndex = statusOrder.indexOf(b.status)
 
   // if both jobs have a status of rejected or withdrew, don't change the order
-  if(aStatusIndex && !bStatusIndex) return 1;
-  if(!aStatusIndex && bStatusIndex) return -1;
+  if(aStatusIndex !== -1 && bStatusIndex === -1) return 1;
+  if(aStatusIndex === -1 && bStatusIndex !== -1) return -1;
   
   // if only one of the jobs has a status of rejected or withdrew, it should come after 
   const aDate = dayjs(a.lastUpdated).utc();
